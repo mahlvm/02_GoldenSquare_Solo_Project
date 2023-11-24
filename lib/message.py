625afec7500992afdacf02f6753d2d5baa4dffca
mahlvm <mahmoreti@hotmail.com>
2023-11-24 17:12:06 +0000

    final version

--- lib/message.py
+++ lib/message.py
@@ -1,8 +1,8 @@
 #import os
 #from twilio.rest import Client
 
-#account_sid = os.environ["ACaa7554b36291e68ab3a4e682de7bd620"]
-#auth_token = os.environ["c8e15d074d8a08133de7c3fb3f1b4a28"]
+#account_sid = os.environ["###"]
+#auth_token = os.environ["###]
 #client = Client(account_sid, auth_token)
 
 class Message:
@@ -13,7 +13,7 @@ class Message:
         message = self.client.messages \
             .create(
                     body = message, 
-                    from_= "+4407506287291",
+                    from_= "+440########",
                     to = phone
                 )
         return message
